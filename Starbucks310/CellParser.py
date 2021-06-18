359786d985c87ab8c6072cd3385f8ae4780bc746
Jared Dyreson <jared.dyreson@gmail.com>
2021-06-18 16:28:35 -0700

    added cleaner parser in CellParser.py

--- Starbucks310/CellParser.py
+++ Starbucks310/CellParser.py
@@ -21,25 +21,28 @@ class Cell:
         self._re = re.compile(textwrap.dedent(self._expression).strip())
         self.match = self._re.match(self.message)
 
-    def create_tuple(self, seeded_date: datetime.datetime) -> tuple[Time.Time]:
         if not(self.match):
-            raise Exception('parsing error')
-        match self.match.groups():
-            case[begin, btod, end, etod, _, _]:
-                return (Time.Time(f'{begin} {btod}', "America/Los_Angeles", seeded_date), Time.Time(f'{end} {etod}', "America/Los_Angeles", seeded_date))
-            case _:
-                raise Exception('parsing error')
-
-    def create_json(self) -> dict:
+            raise Exception('parsing error!')
 
         match(container := self.match.groups()):
-            case[begin, _, end, _, _type, duration]:
-                return {
-                    "begin": begin,
-                    "end": end,
-                    "type": _type,
-                    "duration": duration
-                }
+            case[_, _, _, _, _, _]:
+                self.container = container
+                pass
             case _:
                 raise Exception(
-                    f'expected the following format: [begin, end, _type, duration], received: {container}')
+                    f'parsing error in self.message text: {self.message}')
+
+    def create_tuple(self, seeded_date: datetime.datetime) -> tuple[Time.Time]:
+        begin, btod, end, etod, _, _ = self.container
+        return (Time.Time(f'{begin} {btod}', "America/Los_Angeles", seeded_date),
+                Time.Time(f'{end} {etod}', "America/Los_Angeles", seeded_date))
+
+    def create_json(self) -> dict:
+        begin, _, end, _, _type, duration = self.container
+
+        return {
+            "begin": begin,
+            "end": end,
+            "type": _type,
+            "duration": duration
+        }
